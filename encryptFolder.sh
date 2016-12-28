8a668e78059b529313aff8a4ed83d3c32752af02
alandrieu <alexis.landrieu.pro@gmail.com>
2016-12-28 18:19:34 +0100

    trap openssl error.

--- encryptFolder.sh
+++ encryptFolder.sh
@@ -51,7 +51,8 @@ encryptFile(){
 
 	# Encrypt File name
 	local ENCRYPTED_FILENAME=`echo $fileName | openssl enc -base64 -e -aes-256-cbc -nosalt -pass file:$AESPASSWORD`
-
+	checkError $?
+	
 	# Convert base64 to base64 safe
 	local BASE64_SAFE=`echo "$ENCRYPTED_FILENAME" | tr \/ _`
 	local OUTPUT_FILENAME=$lOUTPUTDIR$BASE64_SAFE
@@ -59,14 +60,17 @@ encryptFile(){
 	
 	# Encrypt 
 	openssl enc -aes-256-cbc -salt -in "$file" -out "$OUTPUT_FILENAME" -pass file:$AESPASSWORD
+	checkError $?
 
 	# Generate signature
 	local lRSA_PRIV_KEY=$WORKSPACE"data_PRIV.pem"
 	local lRSA_PUB_KEY=$WORKSPACE"data_PUB.pem"
 	openssl dgst -passin file:$RSAKEY_PASSWORD_FILE -sha256 -sign "$lRSA_PRIV_KEY" -out "$OUTPUT_FILENAME_SING" "$OUTPUT_FILENAME"
+	checkError $?
 
 	# Check signature
 	openssl dgst -sha256 -verify "$lRSA_PUB_KEY" -signature "$OUTPUT_FILENAME_SING" "$OUTPUT_FILENAME"
+	checkError $?
 
 	echo "> Encrypt $fileName : done"
 	echo "> Output directory : $lOUTPUTDIR"	
@@ -129,7 +133,8 @@ encryptFolderName(){
 
 	# Encrypt File name
 	local ENCRYPTED_FOLDERNAME=`echo $folderName | openssl enc -base64 -e -aes-256-cbc -nosalt -pass file:$AESPASSWORD`
-	
+	checkError $?	
+
 	# Convert base64 to base64 safe
 	local BASE64_SAFE=`echo "$ENCRYPTED_FOLDERNAME" | tr \/ _`
 	local OUTPUT_FOLDERNAME=$lOUTPUTDIR$BASE64_SAFE
@@ -192,9 +197,11 @@ decryptFile()
 
 	# Decrypt filename
 	OUTPUT_FILENAME=`echo $OUTPUT_FILENAME | openssl enc -base64 -d -aes-256-cbc -nosalt -pass file:$AESPASSWORD`
+	checkError $?
 
 	# Decrypt file
 	openssl enc -d -aes-256-cbc -in "$file" -out "$lOUTPUTDIR$OUTPUT_FILENAME" -pass file:$AESPASSWORD
+	checkError $?
 
 	echo "> Decrypt $fileName : done"
 	echo "> Output directory : $lOUTPUTDIR"		
@@ -220,6 +227,7 @@ chekcSingFile()
 
 	# Check signature
 	openssl dgst -sha256 -verify "$lRSA_PUB_KEY" -signature "$OUTPUT_FILENAME_SING" "$file"
+	checkError $?
 
 	echo "> Check signature $fileName : done"	
 }
@@ -292,6 +300,7 @@ decryptFolderName(){
 
 	# Decrypt filename
 	DECRYPTED_FOLDERNAME=`echo $OUTPUT_FOLDERNAME | openssl enc -base64 -d -aes-256-cbc -nosalt -pass file:$AESPASSWORD`
+	checkError $?
 
 	local OUTPUT_FOLDERNAME="$lOUTPUTDIR$DECRYPTED_FOLDERNAME"
 	
@@ -323,6 +332,7 @@ decryptfromString()
 
 	# Decrypt filename
 	OUTPUT_FILENAME=`echo $OUTPUT_FILENAME | openssl enc -base64 -d -aes-256-cbc -nosalt -pass file:$AESPASSWORD`
+	checkError $?
 
 	# Remove secure file	
 	purge $AESPASSWORD
@@ -353,6 +363,7 @@ genkey()
 		read -p "> Your password ? " lRSAKEY_PASSWORD
 		echo $lRSAKEY_PASSWORD > $RSAKEY_PASSWORD_FILE
 		openssl genrsa -aes256 -passout file:$RSAKEY_PASSWORD_FILE -out $lRSA_PRIV_KEY 4096 -noout
+		checkError $?		
 	fi
 
 	# Generate random AES Password
@@ -365,6 +376,7 @@ genkey()
 	else
 		echo "Generate a 256 bit (32 byte) random key"
 		openssl rand -base64 32 > $AESPASSWORD
+		checkError $?
 	fi
 
 	# Export Public key
@@ -372,6 +384,7 @@ genkey()
 	if [ ! -f "$file" ]
 	then
 		openssl rsa -passin file:$RSAKEY_PASSWORD_FILE -pubout -in $lRSA_PRIV_KEY -out $lRSA_PUB_KEY
+		checkError $?		
 	fi
 
 	# Encrypt AES Password
@@ -382,6 +395,7 @@ genkey()
 		exit 1
 	else
 		openssl rsautl -passin file:$RSAKEY_PASSWORD_FILE -encrypt -inkey $lRSA_PUB_KEY -pubin -in $AESPASSWORD -out $lAESPASSWORD_enc
+		checkError $?		
 	fi
 
 	# Remove secure file
@@ -508,8 +522,17 @@ extractkey()
 	if [ ! -f "$file" ]
 	then
 		openssl rsautl -passin file:$RSAKEY_PASSWORD_FILE -decrypt -inkey "$RSA_KEY" -in "$ASEPASS" -out "$file"
+		checkError $?
 	fi
 
+}
+
+checkError()
+{
+	if [ $1 -ne 0 ]; then
+		echo FAIL
+		exit 1
+	fi
 }
 
 purge()
